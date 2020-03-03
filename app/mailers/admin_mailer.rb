e0d7f10dec30a77c5479aaca5454d2ee95d5f308
eliraybon <elr0615@gmail.com>
2020-03-03 18:35:41 -0500

    Update email subject

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,11 @@
 class AdminMailer < ApplicationMailer
-  default to: -> { Admin.pluck(:email) }, from: 'elr0615@gmail.com'
+  default to: -> { Admin.pluck(:email) }
 
   def coin_running_low
     @coin = params[:coin]
-    mail(subject: "#{@coin.name.capitalize.pluralize} are running low!")
+    subject = @coin.quantity.zero? ? 
+      "We're all out of #{@coin.name.capitalize.pluralize}!" :
+      "#{@coin.name.capitalize.pluralize} are running low!"
+    mail(subject: subject)
   end
 end
